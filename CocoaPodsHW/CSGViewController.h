fafdf15e13efa859da21090c04f28cfb0232c9ea
Christina Green <csgreen@gmail.com>
2013-11-12 10:46:42 -0800

    new hw, added weather module

--- CocoaPodsHW/CSGViewController.h
+++ CocoaPodsHW/CSGViewController.h
@@ -10,4 +10,5 @@
 
 @interface CSGViewController : UIViewController
 
+
 @end
--- CocoaPodsHW/CSGViewController.m
+++ CocoaPodsHW/CSGViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "CSGViewController.h"
+#import <JSONKit/JSONKit.h>
 
-@interface CSGViewController ()
+@interface CSGViewController () <NSURLSessionDelegate>
 
 @end
 
@@ -18,6 +19,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+  //  NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+//    NSURLSession *session = [NSURLSession sessionWithConfiguration: configuration
+//                                                          delegate: self
+//                                                     delegateQueue: nil];
+
+    NSData *weatherData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=Seattle,us"]];
+   
+    NSDictionary *weatherDict = [weatherData objectFromJSONData];
+    NSDictionary *currentWeath = [[weatherDict objectForKey:@"weather"] objectAtIndex:0];
+    NSString *weatherString = [currentWeath objectForKey:@"description"];
+    
+    UILabel *weatherLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 80, 200, 40)];
+    [weatherLabel setText:weatherString];
+    [self.view addSubview:weatherLabel];
+    
+    NSLog(@"%@", weatherString);
+    
 }
 
 - (void)didReceiveMemoryWarning
