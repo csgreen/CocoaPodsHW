4a608755bd76823e449d47b4ec1e7acfac4049ba
Christina Green <csgreen@gmail.com>
2013-11-11 20:41:00 -0800

    Initial Commit

---
+++ CocoaPodsHW/CSGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSGAppDelegate.h
+//  CocoaPodsHW
+//
+//  Created by Christina Green on 11/11/13.
+//  Copyright (c) 2013 Greenster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CocoaPodsHW/CSGViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSGViewController.h
+//  CocoaPodsHW
+//
+//  Created by Christina Green on 11/11/13.
+//  Copyright (c) 2013 Greenster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSGViewController : UIViewController
+
+@end
