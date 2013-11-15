004d2ba551090b6a06736710729d23033149a607
Christina Green <csgreen@gmail.com>
2013-11-14 18:47:24 -0800

    added refresh button

--- CocoaPodsHW/CSGViewController.m
+++ CocoaPodsHW/CSGViewController.m
@@ -19,24 +19,65 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-  //  NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
-    
-//    NSURLSession *session = [NSURLSession sessionWithConfiguration: configuration
-//                                                          delegate: self
-//                                                     delegateQueue: nil];
 
+    [self loadViewDesign];
+    
+    UIButton *refreshDataButton = [[UIButton alloc] initWithFrame:CGRectMake(150, 150, 80, 40)];
+    
+    refreshDataButton.backgroundColor = [UIColor redColor];
+    
+    [refreshDataButton addTarget:self action:@selector(refreshData) forControlEvents:UIControlEventTouchUpInside];
+    
+    [refreshDataButton setTitle:@"Refresh" forState:UIControlStateNormal];
+    [self.view addSubview:refreshDataButton];
+    
+    
+}
+
+-(void)loadViewDesign
+{
+    
     NSData *weatherData = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://api.openweathermap.org/data/2.5/weather?q=Seattle,us"]];
-   
+    
     NSDictionary *weatherDict = [weatherData objectFromJSONData];
+    
     NSDictionary *currentWeath = [[weatherDict objectForKey:@"weather"] objectAtIndex:0];
     NSString *weatherString = [currentWeath objectForKey:@"description"];
+    // valueforkeypath
+    NSString *city = [weatherDict objectForKey:@"name"];
+    NSString *cityString = [NSString stringWithFormat:@"In %@, the weather is", city];
     
-    UILabel *weatherLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 80, 200, 40)];
+    NSLog(@"%@", currentWeath);
+    
+    NSDictionary *mainTemp = [weatherDict objectForKey:@"main"];
+    NSString *currentTempInt = [mainTemp objectForKey:@"temp"];
+    
+    NSString *currentTemp = [NSString stringWithFormat:@"Temperature: %@ K", currentTempInt];
+    
+    //    NSString *weatherString1 = [currentTemp objectForKey:@"temp"];
+    
+    
+    
+    UILabel *weatherLabel = [[UILabel alloc] initWithFrame:CGRectMake(20, 100, 200, 40)];
     [weatherLabel setText:weatherString];
     [self.view addSubview:weatherLabel];
     
+    UILabel *weatherLabel1 = [[UILabel alloc] initWithFrame:CGRectMake(20, 80, 200, 40)];
+    [weatherLabel1 setText:cityString];
+    [self.view addSubview:weatherLabel1];
+    
+    UILabel *weatherLabel2 = [[UILabel alloc] initWithFrame:CGRectMake(20, 120, 200, 40)];
+    [weatherLabel2 setText:currentTemp];
+    [self.view addSubview:weatherLabel2];
+    
     NSLog(@"%@", weatherString);
     
+    
+}
+
+-(void)refreshData
+{
+    [self loadViewDesign];
 }
 
 - (void)didReceiveMemoryWarning
